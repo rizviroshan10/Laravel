24472235794e2a688b9fa3d4a6f0c394f5da6625
rizviroshan10 <rizviroshan10@mhs.mdp.ac.id>
2023-04-06 14:25:13 +0700

    menampilkan data

--- rispi/resources/views/fakultas/index.blade.php
+++ rispi/resources/views/fakultas/index.blade.php
@@ -12,11 +12,19 @@
     <thead>
         <tr>
             <th>Nama Fakultas</th>
+            <th>Nama Dekan</th>
+            <th>Nama Wakil Dekan</th>
+            <th>Created At</th>
    </tr>
 </thead>
     <tbody>
 @foreach ($dataFakultas as $item)
-    <tr><td>{{$item}}</td></tr>
+    <tr>
+        <td>{{$item->nama_fakultas}}</td>
+        <td>{{$item->nama_dekan}}</td>
+        <td>{{$item->nama_wakil_dekan}}</td>
+        <td>{{$item->created_at}}</td>
+    </tr>
 @endforeach
 </tbody>
 </table>
